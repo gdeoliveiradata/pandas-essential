31b9844eed6fc3b3a1738b74f68bd8cd21f4dabc
gdeoliveiradata <gdeoliveiradata@gmail.com>
2025-12-19 21:00:41 -0300

    Finished defining the function to download the dataset

--- src/teste.py
+++ src/teste.py
@@ -2,18 +2,17 @@ import requests
 import zipfile
 
 def download_dataset(api_url, zip_path, raw_folder):
-    # Get the binary streamed data from the API
+    # Get the binary data from the API
     with requests.get(api_url, stream=True) as r:
         
         # Raise an error if it occurs
         r.raise_for_status()
 
-        # Write the binary data in chunks to a .zip file
+        # Write the binary data to a .zip file
         with open(zip_path, "wb") as f:
             for chunk in r.iter_content(chunk_size=8192):
                 f.write(chunk)
-    
-    # Extract the .csv file to the raw folder
+
     with zipfile.ZipFile(zip_path, "r") as zip:
         zip.extractall(path=raw_folder)
 
